481212d33907b8370a41f81254e028f7f55c7675
David Souza <dabcs@poli.ufrj.br>
2021-10-18 19:28:18 -0300

    termino do ex

--- src/App.js
+++ src/App.js
@@ -3,17 +3,28 @@ import Home from './components/Home';
 import About from './components/About';
 import Users from './components/Users';
 import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
+import StrictAccess from './StrictAccess';
 
 class App extends Component {
   render() {
+    const user = {
+      username:'joao',
+      password: '1234',
+    }
+
     return (
       <BrowserRouter>
+        <Link to="/strict-access"> strict-access </Link>
         <Link to="/"> Home </Link>
         <Link to="/about"> About </Link>
         <Link to="/users"> Users </Link>
         <Switch>
-          <Route path="/users/:id" render={ (props) => <Users {...props} 
-            greetingsMessage="Good Morning"
+          <Route path="/strict-access"
+            render={(props) => <StrictAccess {...props} user={ user } />}
+          />
+          <Route path="/users/:id" 
+            render={ (props) => <Users {...props} 
+              greetingsMessage="Good Morning"
           /> } />
           <Route path="/about" component={ About } />
           <Route exact path="/" component={ Home } />
--- src/StrictAccess.jsx
+++ src/StrictAccess.jsx
@@ -1,7 +1,26 @@
-import { React, Component } from "react";
+import React, { Component } from "react";
+import { Route } from "react-router-dom"
+import PropTypes from 'prop-types'
+import Home from './components/Home'
 
 export default class StrictAccess extends Component {
-  return (
-    const {} = this.props
-  )
-} 
+  redirect = () => {
+    alert('Access denied');
+    return <Route path='/' component={ Home } />
+  }
+  render() {
+    const { user: { username, password }} = this.props
+    return(
+      <div>
+        {
+          (username === 'joao' 
+            && password === '1234') ? <p>Welcome João</p> : this.redirect()
+        }
+      </div>
+    )
+  }
+}
+
+StrictAccess.propTypes = {
+  user: PropTypes.object,
+}
